7efa631ddcec8b51d676b9889fdc6f48df7eb717
Aakash Jain <aakash@Aakashs-MacBook-Pro.local>
2025-04-17 13:05:45 +0530

    first commit befire DVC

---
+++ test_python_env.py
@@ -0,0 +1,9 @@
+import sys
+
+print("Python version check")
+
+print("Python version: 1", sys.version_info)
+print("Python version: 2", sys.version)
+print()
+# Check where Python is installed
+print("Python executable location: 3", sys.executable)
